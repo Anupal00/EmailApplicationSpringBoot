46a5ba6267b6ea13897ceecab59a39569653f22f
Anupal <patra.anupal@outlook.com>
2022-10-28 12:12:16 +0530

    adding swagger for api documentation.

--- src/main/java/com/example/EmailApplicationSpringBoot/EmailApplicationSpringBootApplication.java
+++ src/main/java/com/example/EmailApplicationSpringBoot/EmailApplicationSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.example.EmailApplicationSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EmailApplicationSpringBootApplication {
 
 	public static void main(String[] args) {
